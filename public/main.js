5c8efa9e93ff98f0516dbae6b9038186bb1554a4
Micahella Estacio <45410954+micahellastc@users.noreply.github.com>
2019-04-01 02:57:12 -0400

    Add files via upload

---
+++ public/main.js
@@ -0,0 +1,18 @@
+const trash = document.getElementsByClassName("fa-trash");
+
+Array.from(trash).forEach(function(element) {
+      element.addEventListener('click', function(){
+        const name = this.parentNode.parentNode.childNodes[1].innerText
+        fetch('messages', {
+          method: 'delete',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            'name': name,
+          })
+        }).then(function (response) {
+          window.location.reload()
+        })
+      });
+});
---
+++ server.js
@@ -0,0 +1,56 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const MongoClient = require('mongodb').MongoClient
+
+var db, collection;
+
+const url = 'mongodb+srv://student:student@cluster0-uxmf2.mongodb.net/test?retryWrites=true';
+const dbName = "student";
+
+app.listen(3000, () => {
+    MongoClient.connect(url, { useNewUrlParser: true }, (error, client) => {
+        if(error) {
+            throw error;
+        }
+        db = client.db(dbName);
+        console.log("Connected to `" + dbName + "`!");
+    });
+});
+
+app.set('view engine', 'ejs')
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  //console.log(db)
+  db.collection('messages').find().toArray((err, result) => {
+    if (err) return console.log(err)
+    console.log(result)
+    res.render('index.ejs', {messages: result})
+  })
+})
+
+app.post('/messages', (req, res) => {
+  db.collection('messages').save({name: req.body.name}, (err, result) => {
+    if (err) return console.log(err)
+    console.log('saved to database')
+    res.redirect('/')
+  })
+})
+
+app.put('/messages', (req, res) => {
+  db.collection('messages')
+  .findOneAndUpdate({name: req.body.name}, (err, result) => {
+    if (err) return res.send(err)
+    res.send(result)
+  })
+})
+
+app.delete('/messages', (req, res) => {
+  db.collection('messages').findOneAndDelete({name: req.body.name}, (err, result) => {
+    if (err) return res.send(500, err)
+    res.send('Message deleted!')
+  })
+})
